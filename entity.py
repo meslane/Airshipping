f2d7a70c74e280da64b288fc71af91133daaa097
meslane <merrickslane@gmail.com>
2022-08-16 00:30:30 -0700

    add spritesheet support

--- entity.py
+++ entity.py
@@ -3,10 +3,54 @@ import pygame
 class Entity(pygame.sprite.Sprite):
     def __init__(self, filepath):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(filepath).convert()
-        self.rect = self.image.get_rect()
         
-    def center_rotate(self, surface, topleft, angle):
-        rotated_image = pygame.transform.rotate(self.image, angle)
-        new_rect = rotated_image.get_rect(center = self.image.get_rect(topleft = topleft).center)
-        surface.blit(rotated_image, new_rect.topleft)
+        self.original_image = pygame.image.load(filepath).convert() #must preserve an undisturbed copy for rotations
+        self.original_rect = self.original_image.get_rect()
+            
+        self.image = self.original_image
+        self.rect = self.original_rect
+        
+    def set_position(self, coords):
+        self.original_rect.topleft = coords
+        self.rect.topleft = coords
+        
+    def draw(self, surface):
+        surface.blit(self.image, self.rect.topleft)
+        
+    def center_rotate(self, angle):
+        self.image = pygame.transform.rotate(self.original_image, angle)
+        self.rect = self.image.get_rect(center = self.original_image.get_rect(topleft = self.original_rect.topleft).center)
+        
+class Spritesheet(pygame.sprite.Sprite):
+    def __init__(self, filepath, spritesize, matrixsize):
+        pygame.sprite.Sprite.__init__(self)
+    
+        self.image = pygame.image.load(filepath).convert_alpha()
+        
+        self.original_surface = pygame.Surface(spritesize).convert_alpha() #must preserve an undisturbed copy for rotations
+        self.original_rect = self.original_surface.get_rect()
+        
+        self.surface = self.original_surface
+        self.rect = self.original_rect
+        
+        self.spritesize = spritesize
+        self.matrixsize = matrixsize
+        
+    def set_position(self, coords):
+        self.original_rect.topleft = coords
+        self.rect.topleft = coords
+        
+    def draw(self, surface):
+        surface.blit(self.surface, self.rect.topleft)
+        
+    def center_rotate(self, angle):
+        self.surface = pygame.transform.rotate(self.original_surface, angle)
+        self.rect = self.surface.get_rect(center = self.original_surface.get_rect(topleft = self.original_rect.topleft).center)
+        
+    def set_sprite(self, index):
+        self.surface.fill((0,0,0,0))
+        self.original_surface.fill((0,0,0,0))
+        x = (index % self.matrixsize[0]) * self.spritesize[0] #get horizontal location of sprite
+        y = (index // self.matrixsize[0]) * self.spritesize[1] #get vertical
+        self.original_surface.blit(self.image, (0,0), (x, y, self.spritesize[0], self.spritesize[1]))
+        self.surface.blit(self.image, (0,0), (x, y, self.spritesize[0], self.spritesize[1]))
--- main.py
+++ main.py
@@ -16,8 +16,16 @@ def main(argv):
     consolas = pygame.font.SysFont("Consolas", 14)
     
     alt = entity.Entity(os.path.join('Art', 'pointer.png'))
-    fuel = entity.Entity(os.path.join('Art', 'pointer.png'))
-    temp = entity.Entity(os.path.join('Art', 'pointer.png'))
+    alt.set_position((599, 293))
+    
+    #amogus = entity.Entity(os.path.join('Art/shitposts', 'Yes.png'))
+    #amogus.set_position((200,200))
+    #fuel = entity.Entity(os.path.join('Art', 'pointer.png'))
+    #temp = entity.Entity(os.path.join('Art', 'pointer.png'))
+    
+    spritetest = entity.Spritesheet(os.path.join('Art/shitposts', 'sussy.png'), (125,105), (5,1))
+    spritetest.set_position((300,100))
+    spritetest.set_sprite(0)
     
     run = True
     fps = 0
@@ -34,9 +42,19 @@ def main(argv):
         screen.blit(frames, (5, 5))
         
         screen.blit(gauges, (640 - 220, 360-74))
-        alt.center_rotate(screen, (599, 293), frame % 300 - 150)
-        fuel.center_rotate(screen, (525, 293), frame % 300 - 150)
-        temp.center_rotate(screen, (451, 293), frame % 300 - 150)
+        
+        alt.center_rotate(frame % 360)
+        alt.draw(screen)
+        
+        spritetest.set_sprite(frame // 10 % 5)
+        spritetest.center_rotate(frame % 360)
+        spritetest.draw(screen)
+        
+        #amogus.center_rotate(frame % 360)
+        #amogus.draw(screen)
+        #alt.center_rotate(screen, (599, 293), frame % 300 - 150)
+        #fuel.center_rotate(screen, (525, 293), frame % 300 - 150)
+        #temp.center_rotate(screen, (451, 293), frame % 300 - 150)
         
         #this goes last in the loop
         window.blit(pygame.transform.scale(screen, window.get_rect().size), (0, 0))
