0c3116d3783f2540d68377f02b10232d0a8531ff
meslane <merrickslane@gmail.com>
2022-08-21 21:09:39 -0700

    finally fixed physics holy shit yes dude

--- entity.py
+++ entity.py
@@ -1,10 +1,16 @@
 import pygame
 import pymunk
+import pymunk.pygame_util
 
 from pathlib import Path
 import typing
 import numpy as np
 import math
+
+def rotate_vec(vector, angle):
+    x = math.cos(angle) * vector[0] - math.sin(angle) * vector[1]
+    y = math.sin(angle) * vector[0] + math.cos(angle) * vector[1]
+    return pymunk.Vec2d(x,y)
 
 class Entity(pygame.sprite.Sprite):
     def __init__(self, space, filepath: Path, **kwargs):
@@ -32,7 +38,7 @@ class Entity(pygame.sprite.Sprite):
         print(self.box.get_vertices())
         self.box.collision_type = 1 
         space.add(self.body, self.box)
-        self.box.density = 0.9
+        self.box.density = 1
         self.box.elasticity = 0.1
         self.box.friction = 0.4
         
@@ -44,21 +50,22 @@ class Entity(pygame.sprite.Sprite):
     def translate(self, offset):
         self.set_position((self.body.position[0] + offset[0], self.body.position[1] + offset[1]))
         
-    def draw(self, drawsurface: pygame.surface):
-        self.rect.topleft = self.body.position
+    def draw(self, space, drawsurface: pygame.surface):
+        #pygame draws from topleft while pymunk draws from center    
+        space.reindex_shape(self.box)
+        space.reindex_shapes_for_body(self.body) 
+        self.rect.topleft = (self.body.position[0] - self.rect.width // 2, self.body.position[1] - self.rect.height // 2)
+        
         if (self.body.angle == 0):
             drawsurface.blit(self.surface, self.rect.topleft)
         else: #rotate and blit if angle != 0 (this is actually better than storing a rotated version persistantly)
-            rotated_surface = pygame.transform.rotate(self.surface, self.body.angle)
+            rotated_surface = pygame.transform.rotate(self.surface, math.degrees(-1* self.body.angle))
             new_rect = rotated_surface.get_rect(center = self.surface.get_rect(topleft = self.rect.topleft).center)
             drawsurface.blit(rotated_surface, new_rect.topleft)
-            pygame.draw.rect(drawsurface, (0, 255, 0), new_rect, 2)
- 
-        pygame.draw.rect(drawsurface, (255, 0, 0), self.rect, 2)
+            #pygame.draw.rect(drawsurface, (0, 255, 0), new_rect, 2)
         
-    def center_rotate(self, space, angle: float):
-        self.body.angle = angle
-        space.reindex_shapes_for_body(self.body) 
+        #pygame.draw.rect(drawsurface, (255, 0, 0), self.rect, 2)
+        #pygame.draw.polygon(drawsurface, (255,0,0), self.box.get_vertices())
         
     def set_sprite_index(self, index: int):
         self.surface.fill((0,0,0,0)) #set transparent
--- main.py
+++ main.py
@@ -34,6 +34,7 @@ class Needles:
 '''
 def collide(arbiter, space, data):
     print("collided")
+    print(data)
     return True
 
 def main(argv):
@@ -44,7 +45,7 @@ def main(argv):
     screen = pygame.Surface((640,360))
     
     space = pymunk.Space()
-    space.gravity = (0,100)
+    space.gravity = (0,400)
     handler = space.add_collision_handler(1,1)
     handler.begin = collide
     
@@ -52,18 +53,36 @@ def main(argv):
     
     #pointers = Needles((320 - 107, 286))
     
-    airship = entity.Entity(space, os.path.join('Art', 'airshipsmall.png'), 
-                            spritesize=(108,64), matrixsize=(2,2), 
-                            mass = 1000, moment = 10000, body_type = pymunk.Body.DYNAMIC)
-    airship.set_position((335,-50))
-    airship.body.velocity = (0, 50)
-    #airship.body.angular_velocity = 10
+    airships = []
     
-    airship2 = entity.Entity(space, os.path.join('Art', 'airshipsmall.png'), 
+    airships.append(entity.Entity(space, os.path.join('Art', 'airshipsmall.png'), 
                             spritesize=(108,64), matrixsize=(2,2), 
-                            body_type = pymunk.Body.KINEMATIC)
-    airship2.set_position((280,300))
-    #airship2.body.velocity = (0, -50)
+                            mass = 1000, moment = 10000, body_type = pymunk.Body.DYNAMIC))
+    airships[0].set_position((200,-50))
+    airships[0].body.velocity = (0, 50)
+    
+    
+    airships.append(entity.Entity(space, os.path.join('Art', 'airshipsmall.png'), 
+                            spritesize=(108,64), matrixsize=(2,2), 
+                            mass = 1000, moment = 10000, body_type = pymunk.Body.DYNAMIC))
+    airships[1].set_position((210,-200))
+    airships[1].body.velocity = (0, 50)
+    
+    airships.append(entity.Entity(space, os.path.join('Art', 'airshipsmall.png'), 
+                            spritesize=(108,64), matrixsize=(2,2), 
+                            mass = 1000, moment = 10000, body_type = pymunk.Body.DYNAMIC))
+    airships[2].set_position((-500,-200))
+    airships[2].body.velocity = (400, 0)
+    #airships[2].body.angle = 45
+    space.reindex_shape(airships[2].box)
+    
+    floor = entity.Entity(space, os.path.join('Art', 'floor.png'),
+                          body_type = pymunk.Body.KINEMATIC)
+    floor.set_position((320,360-8))
+    
+    wall = entity.Entity(space, os.path.join('Art', 'wall.png'),
+                        mass = 10,  body_type = pymunk.Body.DYNAMIC)
+    wall.set_position((320,225))
     
     run = True
     fps = 0
@@ -80,24 +99,29 @@ def main(argv):
         frames = consolas.render("{} fps".format(round(fps,1)),True, (255,255,255))
         screen.blit(frames, (5, 5))
         
-        airship.set_sprite_index(frame // 3 % 4)
-        airship.draw(screen)
+
+        for airship in airships:
+            space.reindex_shape(airship.box)
+            airship.draw(space,screen)
+            print(airship.body.angle)
         
-        #airship2.set_sprite_index(frame // 3 % 4)
-        airship2.draw(screen)
-        #airship.center_rotate(space, frame % 360)
         
+        floor.draw(space, screen)
+        wall.draw(space, screen)
+        '''
         if airship.body.position[1] > 400:
             airship.body.velocity = (-10, -300)
-        
+        '''
         #pointers.set(0, 100 - (frame / 30), 350 - airship.rect.topleft[1] / 3.6)
         #pointers.draw(screen)
         
         #this goes last in the loop
+        #options = pymunk.pygame_util.DrawOptions(screen)
+        #space.debug_draw(options)
         window.blit(pygame.transform.scale(screen, window.get_rect().size), (0, 0))
         pygame.display.flip()
         clock.tick(120)
-        space.step(1.0/60.0)
+        space.step(1.0/240.0)
         
         fps = int(1/(time.time() - startloop + 0.000001))
         frame += 1
